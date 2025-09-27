26e107fb986fb16a78dc466260ee59fa4ec0a1de
abdulsalamahmed9030 <abdulsalamahmed9030@gmail.com>
2025-09-27 20:02:18 +0530

    bismillah

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
   const toggleMenu = () => setIsOpen(!isOpen)
 
   const navLinks = [
-    { label: 'Home', href: '/' },
+    { label: 'dHome', href: '/' },
     { label: 'About', href: '/about-us' },
     { label: 'Services', href: '/services' },
     { label: 'Projects', href: '/portfolio' },
